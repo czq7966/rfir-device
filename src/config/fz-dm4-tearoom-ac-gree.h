5d61397373dc7dc23fcdb66f5fbafebc1af6b10b
陈志谦 <761208@nd.com>
2021-11-10 15:33:56 +0800

    add: ap fun for fz-dm4-tearoom-ac-gree

--- src/config/fz-dm4-tearoom-ac-gree.h
+++ src/config/fz-dm4-tearoom-ac-gree.h
@@ -3,14 +3,24 @@
 
 //OTA
 #define OTA_UPDATE
-#define OTA_VERSION_NUMBER          28
-#define OTA_VERSION_STRING          "28.0"
+#define OTA_VERSION_NUMBER          30
+#define OTA_VERSION_STRING          "30.0"
 #define OTA_UPDATE_URL              "http://betacs.101.com/v0.1/static/preproduction_content_ndcast_ota/ota/fz-dm4-gree/cfg.txt"
 
 
 //WIFI
-#define WIFI_SSID           "MERCURY_95E8"
-#define WIFI_PAWD           ""
+#define WIFI_SSID                   "HUAWEI-House_2.4G"
+#define WIFI_PASSWORD               "like91.nd"
+#define WIFI_RESET_TIMEOUT          60  //多少时间内WIFI未连接，重启，单位秒
+
+//AP
+#define AP_MODE                     true //true时，支持热点配网
+#define AP_SSID                     ""  //为空时，取ChipID
+#define AP_PASSWORD                 "12345678"
+#define AP_RESET_TIMEOUT            60 * 5 //多少时间内WIFI未连接，重启，单位秒
+#define AP_CONFIG_VERSION           "1" //该值若有修改，AP配置将被重置
+#define AP_CONFIG_PIN               -1  //配合AP_CONFIG_PIN_TIMEOUT，长按后进入AP配网模式
+#define AP_CONFIG_PIN_TIMEOUT       5   //配合AP_CONFIG_PIN，长按后进入AP配网模式，单位秒
 
 //MQTT
 #define MQTT_IP                     "172.24.140.41"
