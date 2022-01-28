141896e35644f5cc168ab25167a9de1ea0b9125b
陈志谦 <761208@nd.com>
2022-01-28 14:29:51 +0800

    add: ononoff ota

--- src/config/cl-vrhall-ff-ac-okonoff.h
+++ src/config/cl-vrhall-ff-ac-okonoff.h
@@ -6,15 +6,15 @@
 //OTA
 // #define DISABLE_OTA                 TRUE
 #define OTA_UPDATE
-#define OTA_VERSION_NUMBER          35
-#define OTA_VERSION_STRING          "35.0"
-#define OTA_UPDATE_URL              ""
+#define OTA_VERSION_NUMBER          1
+#define OTA_VERSION_STRING          "1.0"
+#define OTA_UPDATE_URL              "http://betacs.101.com/v0.1/static/preproduction_content_ndcast_ota/ota/cl-vrhall-ff-ac-okonoff/cfg.txt"
 
 
 //WIFI
 // #define DISABLE_WIFI                TRUE
-#define WIFI_SSID                   {"MERCURY_95E8"}
-#define WIFI_PASSWORD               {"12345678"}
+#define WIFI_SSID                   {"MERCURY_95E8", "ND-Guest"}
+#define WIFI_PASSWORD               {"12345678", "88085000"}
 #define WIFI_RESET_TIMEOUT          {30}  //多少时间内WIFI未连接，重启，单位秒
 
 //AP
