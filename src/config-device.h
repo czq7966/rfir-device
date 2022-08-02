a3db72390411baf7c0160e3b471b3ba1e1db24a1
陈志谦 <761208@nd.com>
2022-08-02 18:47:20 +0800

    z3 v4

--- src/config-device.h
+++ src/config-device.h
@@ -40,13 +40,13 @@
 // #define DEVICE_FZ_DM_AC_MIDEA
 
 // 福州亚太美的空调
-#define DEVICE_FZ_YT_AC_MIDEA
+// #define DEVICE_FZ_YT_AC_MIDEA
 
 // 福州亚太格力空调
 // #define DEVICE_FZ_YT_AC_GREE
 
 // Z3 协调器 ESP8266 + CC2652 
-// #define DEVICE_Z3_COORDINATOR_ESP8266_CC2652
+#define DEVICE_Z3_COORDINATOR_ESP8266_CC2652
 
 // RS485透传
 // #define DEVICE_RS485_PENET
