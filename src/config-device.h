38348b1c9abf7dd43db7963efebfe66a5626ffce
陈志谦 <761208@nd.com>
2022-09-19 16:11:30 +0800

    fz-yt-ac-gree:v7

--- src/config-device.h
+++ src/config-device.h
@@ -40,10 +40,10 @@
 // #define DEVICE_FZ_DM_AC_MIDEA
 
 // 福州亚太美的空调
-#define DEVICE_FZ_YT_AC_MIDEA
+// #define DEVICE_FZ_YT_AC_MIDEA
 
 // 福州亚太格力空调
-// #define DEVICE_FZ_YT_AC_GREE
+#define DEVICE_FZ_YT_AC_GREE
 
 // Z3 协调器 ESP8266 + CC2652 
 // #define DEVICE_Z3_COORDINATOR_ESP8266_CC2652
