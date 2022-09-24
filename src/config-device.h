a9bf9f7e2e8622d7457ce1a2e30ef2eaa5121fb6
陈志谦 <761208@nd.com>
2022-09-24 15:39:40 +0800

    rs485-penet:v7

--- src/config-device.h
+++ src/config-device.h
@@ -43,13 +43,13 @@
 // #define DEVICE_FZ_YT_AC_MIDEA
 
 // 福州亚太格力空调
-#define DEVICE_FZ_YT_AC_GREE
+// #define DEVICE_FZ_YT_AC_GREE
 
 // Z3 协调器 ESP8266 + CC2652 
 // #define DEVICE_Z3_COORDINATOR_ESP8266_CC2652
 
 // RS485透传
-// #define DEVICE_RS485_PENET
+#define DEVICE_RS485_PENET
 
 // 8285红外接收发射透传
 // #define DEVICE_RFIR_PENET_IR_8285
