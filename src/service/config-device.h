8b1780cd8311a11a13b5baea319d30b5f4bc1c02
陈志谦 <761208@nd.com>
2022-10-27 22:23:58 +0800

    com-penet v2

--- src/service/config-device.h
+++ src/service/config-device.h
@@ -5,10 +5,10 @@
 // #define DEVICE_BASE
 
 //串口透传
-// #define DEVICE_COM_PENET
+#define DEVICE_COM_PENET
 
 //Z3串口透传
-#define DEVICE_COM_PENET_Z3
+// #define DEVICE_COM_PENET_Z3
 
 //串口半双工透传
 // #define DEVICE_COM_PENET_HALF
